102c832b463e569cf30ae10f3ce2d2f459714939
SilverIkito <ninocourrieu@gmail.com>
2025-12-11 16:12:00 +0100

    Add projet

---
+++ PikminLike/Source/PikminLike/PikminLike.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PikminLike.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PikminLike, "PikminLike" );
